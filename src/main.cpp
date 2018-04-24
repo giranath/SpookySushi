298ab5dad4fbe9e48b7ed9c22527b3482faa8f36
Nathan Giraldeau <Nathan.Giraldeau@USherbrooke.ca>
2018-04-23 20:50:17 -0400

    The programs can load it's configurations

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,25 @@
 #include "../engine/sdl/window.hpp"
 #include "../engine/sdl/event.hpp"
 #include "game_loop.hpp"
+
+#include <toml.hpp>
+#include <fstream>
 #include <SDL.h>
 
-sushi::window create_window() {
-    sushi::window_builder builder("SpookySushi");
+sushi::window create_window(const toml::Table& window_configs) {
+    sushi::window_builder builder(toml::get_or<std::string>(window_configs, "title", "SpookySushi"));
     return builder.with_centered_position()
-            .with_dimensions(800, 600)
+            .with_dimensions(toml::get_or(window_configs, "width", 800), toml::get_or(window_configs, "height", 600))
             .with_opengl()
             .build();
 }
 
 int main() {
+    std::ifstream config_stream("config.toml");
+    auto configs = toml::parse(config_stream);
+
     game_loop loop;
-    sushi::window main_window = create_window();
+    sushi::window main_window = create_window(toml::get<toml::Table>(configs.at("window")));
     loop.run([&main_window]() {
         for(const SDL_Event& ev : sushi::poll_event_iterator{}) {
             if(ev.type == SDL_QUIT) {
