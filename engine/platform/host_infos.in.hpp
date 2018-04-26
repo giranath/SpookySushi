0ae690bcd487e55abb10cd23a39e00f9c9167e27
Nathan Giraldeau <Nathan.Giraldeau@USherbrooke.ca>
2018-04-26 08:03:53 -0400

    The cache line size can be specified to prevent false sharing

---
+++ engine/platform/host_infos.in.hpp
@@ -0,0 +1,12 @@
+#ifndef SPOOKYSUSHI_HOST_INFOS_HPP
+#define SPOOKYSUSHI_HOST_INFOS_HPP
+
+#include <cstddef>
+
+namespace sushi {
+
+const std::size_t CACHE_LINE_SIZE = ${SUSHI_CACHE_LINE_SIZE};
+
+}
+
+#endif
