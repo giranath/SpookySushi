24ba182d0d487c79975e250bd4ef7d84ff2509ed
Nathan Giraldeau <Nathan.Giraldeau@USherbrooke.ca>
2018-05-03 17:20:17 -0400

    Removed test code

--- src/main.cpp
+++ src/main.cpp
@@ -47,37 +47,6 @@ const uint32_t JOB_PROFILE_NAME = 315135;
 const uint32_t FRAME_PROFILE_NAME = 666;
 
 int main() {
-    sushi::ecs::indirect_vector_storage<int> integers_storage;
-    sushi::ecs::tree_storage<double> doubles_storage;
-    sushi::ecs::tree_storage<float> floats_storage;
-
-    integers_storage.emplace(12, 3);
-    doubles_storage.emplace(12, 3.14159);
-    floats_storage.emplace(12, 3.14159f);
-
-    integers_storage.emplace(2, 351);
-    doubles_storage.emplace(2, 14.2);
-    floats_storage.emplace(2, 3.14159f);
-
-    for(auto p : sushi::ecs::entity_iterator(integers_storage, doubles_storage, floats_storage)) {
-        int a;
-        double b;
-        float c;
-
-        std::tie(a, b, c) = p.second;
-        std::cout << p.first << " " << a << ", " << b << ", " << c << std::endl;
-        std::get<1>(p.second) = 0.0;
-    }
-
-    for(auto p : sushi::ecs::entity_iterator(integers_storage, doubles_storage, floats_storage)) {
-        int a;
-        double b;
-        float c;
-
-        std::tie(a, b, c) = p.second;
-        std::cout << p.first << " " << a << ", " << b << ", " << c << std::endl;
-    }
-
     std::ifstream config_stream("asset/config.toml");
     auto configs = toml::parse(config_stream);
 
