56867809c754d7b1682690c080ad5951b5da53e8
Nathan Giraldeau <nathan.giraldeau@usherbrooke.ca>
2018-07-07 11:19:10 -0400

    Glm is now an external project

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -33,7 +33,7 @@ file(MAKE_DIRECTORY ${DEPENDENCIES_ROOT}/toml11)
 ExternalProject_Add(toml11
         URL "https://github.com/ToruNiina/toml11/archive/v1.0.1.zip"
         CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=${DEPENDENCIES_ROOT}/toml11"
-        INSTALL_DIR "${DEPENDENCIES_ROOT}/SDL2"
+        INSTALL_DIR "${DEPENDENCIES_ROOT}/toml11"
         INSTALL_COMMAND "${CMAKE_COMMAND}" -E copy ../toml11/toml.hpp "${DEPENDENCIES_ROOT}/toml11"
                         COMMAND "${CMAKE_COMMAND}" -E copy_directory ../toml11/toml "${DEPENDENCIES_ROOT}/toml11/toml")
 add_library(libToml11 INTERFACE IMPORTED GLOBAL)
@@ -41,3 +41,18 @@ add_dependencies(libToml11 toml11)
 
 set_target_properties(libToml11 PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES "${DEPENDENCIES_ROOT}/toml11/")
+
+
+#=======================================================================================================================
+# glm
+#=======================================================================================================================
+file(MAKE_DIRECTORY ${DEPENDENCIES_ROOT}/glm)
+ExternalProject_Add(glm
+    URL https://github.com/g-truc/glm/archive/0.9.9.0.zip
+    CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=${DEPENDENCIES_ROOT}/glm"
+    INSTALL_DIR "${DEPENDENCIES_ROOT}/glm")
+add_library(libGlm INTERFACE IMPORTED GLOBAL)
+add_dependencies(libGlm glm)
+
+set_target_properties(libGlm PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${DEPENDENCIES_ROOT}/glm/include")
