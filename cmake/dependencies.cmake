9b2819b09d8d2bf75fad294df9813582e0fae0e1
Nathan Giraldeau <nathan.giraldeau@usherbrooke.ca>
2018-07-07 10:33:10 -0400

    SDL2 is now an external project

---
+++ cmake/dependencies.cmake
@@ -0,0 +1,24 @@
+include(ExternalProject)
+
+set(DEPENDENCIES_ROOT "${CMAKE_BINARY_DIR}/dependencies")
+
+#=======================================================================================================================
+# SDL2
+#=======================================================================================================================
+ExternalProject_Add(SDL2
+        URL "https://www.libsdl.org/release/SDL2-2.0.8.zip"
+        CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=${DEPENDENCIES_ROOT}/SDL2"
+        INSTALL_DIR "${DEPENDENCIES_ROOT}/SDL2")
+
+# Create a target to reference the external project
+add_library(libSDL2 IMPORTED SHARED GLOBAL)
+add_library(libSDL2main IMPORTED STATIC GLOBAL)
+add_dependencies(libSDL2 SDL2)
+add_dependencies(libSDL2main SDL2)
+
+set_target_properties(libSDL2 PROPERTIES
+        "IMPORTED_LOCATION" "${DEPENDENCIES_ROOT}/SDL2/lib/${CMAKE_SHARED_LIBRARY_PREFIX}SDL2${CMAKE_SHARED_LIBRARY_SUFFIX}"
+        "INTERFACE_INCLUDE_DIRECTORIES" "${DEPENDENCIES_ROOT}/SDL2/include/SDL2")
+set_target_properties(libSDL2main PROPERTIES
+        "IMPORTED_LOCATION" "${DEPENDENCIES_ROOT}/SDL2/lib/${CMAKE_STATIC_LIBRARY_PREFIX}SDL2main${CMAKE_STATIC_LIBRARY_SUFFIX}"
+        "INTERFACE_INCLUDE_DIRECTORIES" "${DEPENDENCIES_ROOT}/SDL2/include/SDL2")
