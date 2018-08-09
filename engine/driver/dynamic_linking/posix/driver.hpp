333d206864e81e6ae8d1874099df84d287f0e276
Nathan Giraldeau <nathan.giraldeau@usherbrooke.ca>
2018-08-09 18:54:23 -0400

    Started to write driver for shared library

---
+++ engine/driver/dynamic_linking/posix/driver.hpp
@@ -0,0 +1,16 @@
+#ifndef SPOOKYSUSHI_DRIVER_DYNLINK_HPP
+#define SPOOKYSUSHI_DRIVER_DYNLINK_HPP
+
+namespace sushi { namespace driver { namespace dl {
+
+using ObjectHandle = void*;
+using SymbolPtr = void*;
+
+ObjectHandle open(const char* path);
+int close(ObjectHandle object);
+
+SymbolPtr get_symbol(ObjectHandle object, const char* name);
+
+}}}
+
+#endif
---
+++ engine/driver/dynamic_linking/windows/driver.hpp
@@ -0,0 +1,8 @@
+#ifndef SPOOKYSUSHI_DRIVER_DYNLINK_HPP
+#define SPOOKYSUSHI_DRIVER_DYNLINK_HPP
+
+namespace sushi { namespace driver {
+
+}}
+
+#endif
