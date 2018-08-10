54873fba7f592fefccf4f3dd64d3c616304068b9
Nathan Giraldeau <Nathan.Giraldeau@USherbrooke.ca>
2018-08-10 06:25:17 -0400

    Started to write the package serialization support

--- engine/asset/private/package.cpp
+++ engine/asset/private/package.cpp
@@ -1,6 +1,48 @@
 #include "package.hpp"
 
+#include <cstring>
+
 namespace sushi {
 
+std::istream& PackageHeader::loadFromStream(std::istream& input_stream, PackageHeader& package) {
+    char magic[4];
+    input_stream.read(magic, sizeof(magic));
+    for(int i = 0; i < 4; ++i) {
+        if(magic[i] != PackageHeader::Magic[i]) {
+            input_stream.setstate(std::ios::failbit);
+            return input_stream;
+        }
+    }
+
+    input_stream.read(reinterpret_cast<char*>(package.flags), 2);
+    input_stream.read(reinterpret_cast<char*>(package.version), 1);
+    input_stream.ignore(1);
+
+    return input_stream;
+}
+
+std::ostream& PackageHeader::saveToStream(std::ostream& output_stream, const PackageHeader& package) {
+    return output_stream;
+}
+
+bool Package::is_valid() const noexcept {
+    return is_valid_;
+}
+
+std::istream& Package::loadFromStream(std::istream& input_stream, Package& package) {
+    package.is_valid_ = false;
+
+    PackageHeader header;
+    if(!PackageHeader::loadFromStream(input_stream, header)) {
+        return input_stream;
+    }
+
+    package.is_valid_ = true;
+    return input_stream;
+}
+
+std::ostream& Package::saveToStream(std::ostream& output_stream, const Package& package) {
+    return output_stream;
+}
 
 }
--- engine/asset/public/package.hpp
+++ engine/asset/public/package.hpp
@@ -3,29 +3,45 @@
 
 #include "package_types.hpp"
 #include <cstdint>
+#include <vector>
+#include <iostream>
 
 namespace sushi {
 
-class PackageHeader {
-    // A package magic
+struct PackageHeader {
     static constexpr char Magic[4] = {'S', 'A', 'P', 'K'};
     enum {
         MaskLittleEndian = 0x1
     };
 
-    uint16_t flags;
-    uint8_t version;
+    uint16_t flags {};
+    uint8_t version {};
+
+    PackageHeader() = default;
+
+    static std::istream& loadFromStream(std::istream& input_stream, PackageHeader& package);
+    static std::ostream& saveToStream(std::ostream& output_stream, const PackageHeader& package);
 };
 
-class PackageEntryTable {
-
+struct PackageEntry {
+    uint16_t identifier;
+    PackageType type;
+    uint64_t offset;
+    uint64_t size;
+    uint32_t checksum;
+    uint64_t reserved;
 };
 
 // A package hold
 class Package {
-    PackageHeader header;
-    PackageEntryTable entries;
+    std::vector<PackageEntry> entries;
     uint8_t* content;
+    bool is_valid_;
+public:
+    bool is_valid() const noexcept;
+
+    static std::istream& loadFromStream(std::istream& input_stream, Package& package);
+    static std::ostream& saveToStream(std::ostream& output_stream, const Package& package);
 };
 
 }
